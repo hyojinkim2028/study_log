ed1eb3c004e4df7eb24583e405c191f10ffd4767
rizzy <12hyojinkim14@gmail.com>
2023-10-09 18:43:48 +0900

    add edit/del

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-// Firebase SDK 라이브러리 가져오기
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js'
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js'
-import {
-  doc,
-  collection,
-  addDoc,
-  updateDoc,
-  setDoc,
-} from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js'
-import { getDocs } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js'
-
-// Firebase 구성 정보 설정
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+function createAt() {
+  let date = new Date()
+  let time = date.toLocaleString()
+  return time
+}
 const firebaseConfig = {
   apiKey: 'AIzaSyCFGVHQxjx1VHGJPr7p04pJYT2V9qSgBWE',
   authDomain: 'sparta-b3aff.firebaseapp.com',
@@ -22,9 +13,8 @@ const firebaseConfig = {
   measurementId: 'G-9FNSVJPZX2',
 }
 
-// Firebase 인스턴스 초기화
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
+firebase.initializeApp(firebaseConfig)
+database = firebase.firestore()
 
 // posting toggle
 
@@ -34,6 +24,127 @@ $('#postingbtn').click(async function () {
   $('#postingbox').toggle()
 })
 
+// 공부 데이터 가져오기
+const docRef = database
+  .collection('study-log')
+  .orderBy('createdAt', 'desc')
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      const data = doc.data()
+      let id = doc.id
+      let date = data.date
+      let goal = data.goal
+      let achievement = data.achievement
+      let link = data.link
+      let etc = data.etc
+      let understanding = data.understanding
+
+      let temp_html = `
+          <tr data-documentId=${id} data-collectionName="study-log">
+              <td>${date}</td>
+              <td>${goal}</td>
+              <td>${achievement}</td>
+              <td>${link}</td>
+              <td>${etc}</td>
+              <td>${understanding}</td>
+              <td><button class="edit">edit</button></td>
+              <td><button class="del">del</button></td>
+          </tr>
+          `
+      $('#table').append(temp_html)
+    })
+    // 수정
+
+    const editComment = document.querySelectorAll('.edit')
+    editComment.forEach((ele) => {
+      ele.addEventListener('click', async (e) => {
+        // edit 버튼 클릭
+        // 수정창 팝업됨
+        $('#editPopup').css({
+          top:
+            ($(window).height() - $('#editPopup').outerHeight()) / 2 +
+            $(window).scrollTop() +
+            'px',
+          left:
+            ($(window).width() - $('#editPopup').outerWidth()) / 2 +
+            $(window).scrollLeft() +
+            'px',
+        })
+        $('#editPopup_mask').css('display', 'block')
+        $('#editPopup').css('display', 'block')
+        $('body').css('overflow', 'hidden')
+
+        let id = e.target.closest('tr').dataset.documentid
+        let editLog = database.collection('study-log').doc(id)
+        editLog.get().then((doc) => {
+          let data = doc.data()
+          let date = data.date
+          let goal = data.goal
+          let achievement = data.achievement
+          let link = data.link
+          let etc = data.etc
+          let understanding = data.understanding
+
+          $('#edit-date').val(date)
+          $('#edit-goal').text(goal)
+          $('#edit-achievement').text(achievement)
+          $('#edit-link').text(link)
+          $('#edit-etc').text(etc)
+          $('#edit-understanding').val(understanding)
+
+          $('.edit-submit').click(function (event) {
+            var editRef = database.collection('study-log').doc(id)
+            return editRef
+              .update({
+                date: $('#edit-date').val(),
+                goal: $('#edit-goal').val(),
+                achievement: $('#edit-achievement').val(),
+                link: $('#edit-link').val(),
+                etc: $('#edit-etc').val(),
+                understanding: $('#edit-understanding').val(),
+              })
+              .then(() => {
+                console.log('수정완료!')
+                window.location.reload()
+              })
+              .catch((error) => {
+                console.error('에러: ', error)
+              })
+          })
+        })
+      })
+    })
+    // 닫기 버튼 클릭시
+    $('#close').click(function (event) {
+      $('#editPopup_mask').css('display', 'none')
+      $('#editPopup').css('display', 'none')
+      $('body').css('overflow', 'auto')
+    })
+
+    // 데이터 삭제
+
+    const deleteLog = document.querySelectorAll('.del')
+    deleteLog.forEach((ele) => {
+      ele.addEventListener('click', async (e) => {
+        let id = e.target.closest('tr').dataset.documentid
+        let password = prompt('비밀번호 입력')
+        if (password == 4646) {
+          database
+            .collection('study-log')
+            .doc(id)
+            .delete()
+            .then(() => {
+              window.location.reload() // 페이지 새로고침
+            })
+            .catch((error) => {
+              console.error('에러: ', error)
+            })
+        }
+      })
+    })
+  })
+
 // 공부 데이터 저장
 $('#submit').click(async function () {
   let date = $('#date').val()
@@ -42,44 +153,25 @@ $('#submit').click(async function () {
   let link = $('#link').val()
   let etc = $('#etc').val()
   let understanding = $('#understanding').val()
+  let createdAt = createAt()
 
-  let doc = {
-    date: date,
-    goal: goal,
-    achievement: achievement,
-    link: link,
-    etc: etc,
-    understanding: understanding,
-  }
-
-  await addDoc(collection(db, 'study-log'), doc)
-  alert('저장되었습니다.')
-  console.log(doc)
-  //window.location.reload()
-})
-
-// 공부 데이터 가져오기
-let docs = await getDocs(collection(db, 'study-log'))
-docs.forEach((doc) => {
-  let data = doc.data()
-
-  let date = data.date
-  let goal = data.goal
-  let achievement = data.achievement
-  let link = data.link
-  let etc = data.etc
-  let understanding = data.understanding
-
-  let temp_html = `
-        <tr><td class="space"></td></tr>
-        <tr>
-            <td>${date}</td>
-            <td>${goal}</td>
-            <td>${achievement}</td>
-            <td>${link}</td>
-            <td>${etc}</td>
-            <td>${understanding}</td>
-        </tr>
-        `
-  $('#table').append(temp_html)
+  database
+    .collection('study-log')
+    .add({
+      date,
+      goal,
+      achievement,
+      link,
+      etc,
+      understanding,
+      createdAt,
+    })
+    .then(() => {
+      console.log('성공')
+      alert('저장되었습니다.')
+      window.location.reload()
+    })
+    .catch((error) => {
+      console.error('실패')
+    })
 })
